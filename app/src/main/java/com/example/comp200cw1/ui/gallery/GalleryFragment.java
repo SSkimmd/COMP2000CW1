9595cf2a12b848e50855c07f1eb358eeff040641
TB <87049886+SSkimmd@users.noreply.github.com>
2022-11-22 11:09:54 +0000

    integrated toast notifications for submitting leave applications

--- app/src/main/java/com/example/comp200cw1/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/comp200cw1/ui/gallery/GalleryFragment.java
@@ -4,21 +4,44 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.example.comp200cw1.R;
 import com.example.comp200cw1.databinding.FragmentGalleryBinding;
 
 public class GalleryFragment extends Fragment {
-
-private FragmentGalleryBinding binding;
+    private FragmentGalleryBinding binding;
+    private boolean toggleWarning = false;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         GalleryViewModel galleryViewModel = new ViewModelProvider(this).get(GalleryViewModel.class);
 
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        Button selectDateButton = root.findViewById(R.id.button);
+        selectDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(toggleWarning) {
+                    Toast warning = Toast.makeText(root.getContext(), "Date(s) Unavailable", Toast.LENGTH_SHORT);
+                    warning.show();
+                } else {
+                    Toast applied = Toast.makeText(root.getContext(), "Application Submitted", Toast.LENGTH_SHORT);
+                    applied.show();
+                }
+
+                toggleWarning = !toggleWarning;
+            }
+        });
+
+
         return root;
     }
 
