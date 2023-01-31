23acc1c12d2187754759532415765102594d37ce
SemISkimmdMilk <jimmybob7860@gmail.com>
2023-01-31 09:22:14 +0000

    added login, update and logout systems

--- app/src/main/java/com/example/comp200cw1/EditDetailsFragment.java
+++ app/src/main/java/com/example/comp200cw1/EditDetailsFragment.java
@@ -13,18 +13,8 @@ import android.view.ViewGroup;
  * Use the {@link nav_edit_details#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class nav_edit_details extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public nav_edit_details() {
+public class EditDetailsFragment extends Fragment {
+    public EditDetailsFragment() {
         // Required empty public constructor
     }
 
